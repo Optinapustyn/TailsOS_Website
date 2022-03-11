4ba8143467b2e016827c83e2b902f3c48867c69a
Mert Gunduz <merttgg@gmail.com>
2022-03-11 14:08:14 +0300

    Source Added

---
+++ languagedivscript.js
@@ -0,0 +1,67 @@
+const englishButton = document.querySelector("#language-button-en");
+const deutschButton = document.querySelector("#language-button-de");
+const espanolButton = document.querySelector("#language-button-es");
+const frenchButton = document.querySelector("#language-button-fr");
+const italianButton = document.querySelector("#language-button-it");
+const portugueseButton = document.querySelector("#language-button-pt");
+const russianButton = document.querySelector("#language-button-ru");
+
+// SHOW LANGUAGE ALL START
+function showEnglish() {
+      englishButton.textContent = "English";
+}
+
+function showGerman() {
+      deutschButton.textContent = "Deutsch";
+}
+
+function showEspanol() {
+      espanolButton.textContent = "Español";
+}
+
+function showFrench() {
+      frenchButton.textContent = "Français";
+}
+
+function showItalian() {
+      italianButton.textContent = "Italiano";
+}
+
+function showPortuguese() {
+      portugueseButton.textContent = "Português";
+}
+
+function showRussian() {
+      russianButton.textContent = "Русский";
+}
+// SHOW LANGUAGE ALL END
+
+// SHOW LANGUAGE ABBREVIATION START
+function abbrEnglish() {
+      englishButton.textContent = "English";
+}
+
+function abbrGerman() {
+      deutschButton.textContent = "DE";
+}
+
+function abbrEspanol() {
+      espanolButton.textContent = "ES";
+}
+
+function abbrFrench() {
+      frenchButton.textContent = "FR";
+}
+
+function abbrItalian() {
+      italianButton.textContent = "IT";
+}
+
+function abbrPortuguese() {
+      portugueseButton.textContent = "PT";
+}
+
+function abbrRussian() {
+      russianButton.textContent = "RU";
+}
+// SHOW LANGUAGE ABBREVIATION END
